5be71017213416128b5dc03a99e853b117d987f5
abstylemedia <abstylemedia@gmail.com>
2022-06-26 19:33:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './components/navbar';
+import './App.css';
+import Main from './components/main';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
